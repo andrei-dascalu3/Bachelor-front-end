dda67d688529df468d16de41c0eaa54f04d7973f
andrei-dascalu3 <andrei.dascalu2@gmail.com>
2022-12-05 14:58:32 +0200

    Added sorting option to table

--- src/app/users/user-list-alt/user-list-alt.component.ts
+++ src/app/users/user-list-alt/user-list-alt.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -20,7 +21,9 @@ export class UserListAltComponent implements OnInit, AfterViewInit {
   users: User[];
   subscription: Subscription;
   dataSource: any;
+  sortedData: User[];
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) matSort: MatSort;
 
   constructor(
     private userService: UserService,
@@ -28,10 +31,30 @@ export class UserListAltComponent implements OnInit, AfterViewInit {
     private route: ActivatedRoute
   ) {}
 
-  getPageDetails(event:any) {
-    console.log(event);
+  getPageDetails(event: any) {}
+
+  sortData(sort: Sort) {
+    const data = this.users.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name':
+          return compare(a.lastName, b.lastName, isAsc);
+        case 'email':
+          return compare(a.email, b.email, isAsc);
+        case 'type':
+          return compare(a.type, b.type, isAsc);
+        default:
+          return 0;
+      }
+    });
   }
-  
+
   ngOnInit(): void {
     this.subscription = this.userService.usersChanged.subscribe(
       (users: User[]) => {
@@ -43,10 +66,15 @@ export class UserListAltComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort=this.matSort;
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
